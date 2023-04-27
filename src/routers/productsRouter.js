730245e3d507917261392c4e4bc3456285aa9267
PauloBrandino <contatobrandino@gmail.com>
2023-04-27 18:39:51 -0300

    fix lint

--- src/routers/productsRouter.js
+++ src/routers/productsRouter.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const { productController } = require('../controllers');
+
 router.get('/', productController.getAll);
 
 router.get('/:id', productController.getById);
